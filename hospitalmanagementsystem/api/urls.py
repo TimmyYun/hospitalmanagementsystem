9a2671ad2789e04aac1ec32969598df6f6aabd27
Timmy Yun <timur.unaspekov@nu.edu.kz>
2022-11-02 14:27:37 +0600

    Cleared project

--- hospitalmanagementsystem/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.getRoutes, name="routes"),
-    path('appointments/', views.getAppointments, name="appointments"),
-    path('register/', views.registerPage, name="register"),
-    path('example/', views.example_view, name="example")
-]
